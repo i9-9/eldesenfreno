28150a26c93376d223b6957137b85f102277baff
Flor <florencgmz@gmail.com>
2024-01-22 13:51:27 -0300

    edition item component

---
+++ app/components/EdItem.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface EdItemProps {
+    image: string;
+    title: string;
+    author: string;
+    review: string;
+    reviewName: string;
+    link: string;
+}
+const EdItem = ({image, title, author, review, reviewName, link}: EdItemProps) => {
+
+  return (
+    <div className='m-2 md:w-96'>
+        <Image className='border border-white' src={image} alt={title} width={300} height={400}/>
+        <h4 className='uppercase font-semibold mb-2'>{title}</h4>
+        <h5 className='uppercase text-xs mb-2'>{author}</h5>
+        <p className='text-xs mb-2'>{review}</p>
+        <p className='text-xs italic mb-4'>{reviewName}</p>
+        <div className='flex'>
+            <button className='border border-white rounded-md p-2 text-xs mr-2'>Saber más</button>
+            <button className='border border-white rounded-md p-2 text-xs'>Comprar</button>
+        </div>
+    </div>
+  )
+}
+
+export default EdItem
---
+++ app/editions.tsx
@@ -0,0 +1,22 @@
+const editions = [
+    {
+        image: '/1.png',
+        title: 'Láudano para el corazón negro',
+        author: 'María Magdalena',
+        review: `Hay escrituras que respiran entre las grietas producidas por su carácter disruptivo. María Magdalena lo demuestra en cada libro, desacomoda las formas hasta que entonen un canto singular. Ahí encuentra su descanso. En Láudano para el corazón negro, nos entrega un extenso poema fragmentado, acuchillado: ¿Y si fuiste herida en el río? // ¿Y si dijeron las palabras / cuchillos / las palabras filo / una tarde de verano / en el río? /. Cada escena del crimen nos conduce a otra en una especie de conjuro que retorna y punza. Un conjuro que busca un antídoto donde ponernos a salvo en este escenario tan lírico como cruel.`,
+        reviewName: 'Enzo Amarillo',
+        link: ''
+    },
+    {
+        image: '/2.png',
+        title: 'Harán una película con mi entierro',
+        author: 'María Malusardi',
+        review: `El lenguaje permite lo imposible. El yo asiste a su funeral y sostiene la voz. En la ceremonia, los verbos sacan las mortajas al sol y cantan. El futuro es puro lenguaje: está hecho de acciones que se lanzan hacia adelante como serpentinas de deseo, furia o dolor. La vida después es un estribillo que repiquetea en los días por venir. En el entierro de la obra, el poema labra el testamento, y también ríe y baila sobre su tumba. Es imposible no hacerse la película. La sábana del lecho es la pantalla en la que pasará la vida: ojos, roces, harapos, huellas, hijos. Un campanario y los zapatos. ¿Qué harán con la desnudez y la soledad? ¿Adónde irán las esquinas y las lápidas?
+        Con voz acicalada de humor y finitud, María Malusardi nos acerca a los efectos secundarios de la muerte y ofrece —para quienes no sabemos morir ni rezar— un conjunto de poemas sobre esa loca fantasía del final.`,
+        reviewName: 'Gabriela Franco',
+        link: ''
+    },
+
+];
+
+export default editions;
