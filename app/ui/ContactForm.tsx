6a6426e0cfb7e0674daabde69ecd539ef87f688a
i9-9 <yvan.vrs@gmail.com>
2024-08-28 19:07:36 -0300

    fixing the contact form

--- app/ui/ContactForm.tsx
+++ app/ui/ContactForm.tsx
@@ -8,9 +8,9 @@ const ContactForm = () => {
         <p className="text-xs text-white mb-2 font-bold">
           Dudas, consultas, propuestas
         </p>
-        <Link href="mailto:eldesenfreno@gmail.com" className="text-white py-2 hover:underline text-xs">
+        <Link href="mailto:eldesenfreno.contacto@gmail.com" className="text-white py-2 hover:underline text-xs">
         <button type="submit" className="p-2 bg-[#2C2C2C] hover:bg-gray-400 transition-all duration-700 drop-shadow opacity-80 text-xs w-full bg-opacity-10 border border-white border-opacity-20 rounded-lg">
-          eldesenfreno@gmail.contacto
+          eldesenfreno.contacto@gmail.com
         </button>
         </Link>
       </div>
