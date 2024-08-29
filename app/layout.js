11983be7fcc5e19a4cb3b3b392efb90df1cf8037
i9-9 <yvan.vrs@gmail.com>
2024-08-29 12:25:36 -0300

    favicon added

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import MenuHeader from "./components/MenuHeader";
 import MobileHeader from "./components/MobileHeader";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 const aggie = localFont({
   src: [
@@ -24,14 +25,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={aggie.className} style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body
+        className={aggie.className}
+        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
         <MenuHeader />
         <MobileHeader />
         <div className="flex flex-1 flex-col">
           <Sidebar />
-          <main className="md:ml-[500px] md:mt-10 md:flex-1">
-            {children}
-          </main>
+          <main className="md:ml-[500px] md:mt-10 md:flex-1">{children}</main>
         </div>
         {/* <Footer /> */}
       </body>
