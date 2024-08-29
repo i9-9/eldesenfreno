141caae379114f92f0b933023acebede3832550f
i9-9 <yvan.vrs@gmail.com>
2024-08-29 11:00:57 -0300

    emailjs now implemented

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,12 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
 import ContactForm from '../ui/ContactForm'
 
 const Contact = () => {
   return (
-    <div className='flex flex-col ml-2 mr-8 text-sm mt-4 font-neue-display ' >
+    <div className='flex flex-col ml-2 mr-8 text-sm mt-4 font-neue-display' >
       <ContactForm />
     </div>
   )
--- app/ui/ContactForm.tsx
+++ app/ui/ContactForm.tsx
@@ -1,47 +1,92 @@
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
 import Link from 'next/link';
-import React from 'react';
 
 const ContactForm = () => {
+  // Define estados separados para cada campo del formulario
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [sending, setSending] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSending(true);
+
+    // Utiliza emailjs para enviar el formulario
+    emailjs.sendForm('service_jo40tzl', 'template_5fep019', e.target, 'cWBowbfYgWXQ64nWu')
+      .then((result) => {
+        console.log(result.text);
+        setSuccess(true);
+        // Limpia los campos del formulario
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.log(error.text);
+        setSuccess(false);
+      })
+      .finally(() => {
+        setSending(false);
+      });
+  };
+
   return (
     <div className="flex flex-col items-start pt-2 px-2 max-w-lg mx-0 rounded-lg shadow-lg">
       <div className='flex flex-col'>
         <p className="text-xs text-white mb-2 font-bold">
           Dudas, consultas, propuestas
         </p>
-        <Link href="mailto:eldesenfreno.contacto@gmail.com" className="text-white py-2 hover:underline text-xs">
-        <button type="submit" className="p-2 bg-[#2C2C2C] hover:bg-gray-400 transition-all duration-700 drop-shadow opacity-80 text-xs w-full bg-opacity-10 border border-white border-opacity-20 rounded-lg">
-          eldesenfreno.contacto@gmail.com
-        </button>
+        <Link href="mailto:eldesenfreno.contacto@gmail.com">
+          <button type="button" className="p-2 bg-[#2C2C2C] hover:bg-gray-400 transition-all duration-700 text-xs w-full text-white border border-white border-opacity-20 rounded-lg">
+            eldesenfreno.contacto@gmail.com
+          </button>
         </Link>
       </div>
-      <form className="w-full flex flex-col text-xs mt-4">
+      <form className="w-full flex flex-col text-xs mt-4" onSubmit={handleSubmit}>
         <label className="text-white mb-2" htmlFor="name">Nombre:</label>
         <input
           type="text"
           id="name"
-          name="name"
+          name="from_name"
+          value={name} // Usa el estado `name` como el valor del input
+          onChange={(e) => setName(e.target.value)} // Actualiza el estado `name`
           className="p-2 mb-4 bg-[#121212] border border-white border-opacity-20 rounded-md text-white focus:border-gray-400 focus:outline-none"
           placeholder="Su nombre"
+          required
         />
         <label className="text-white mb-2" htmlFor="email">Correo electrónico:</label>
         <input
           type="email"
           id="email"
-          name="email"
+          name="from_email"
+          value={email} // Usa el estado `email` como el valor del input
+          onChange={(e) => setEmail(e.target.value)} // Actualiza el estado `email`
           className="p-2 mb-4 bg-[#121212] border border-white border-opacity-20 rounded-md text-white focus:border-gray-400 focus:outline-none"
           placeholder="Su correo electrónico"
+          required
         />
         <label className="text-white mb-2" htmlFor="message">Mensaje:</label>
         <textarea
           id="message"
           name="message"
+          value={message} // Usa el estado `message` como el valor del textarea
+          onChange={(e) => setMessage(e.target.value)} // Actualiza el estado `message`
           rows={4}
           className="p-2 mb-4 bg-[#121212] border border-white border-opacity-20 rounded-md text-white focus:border-gray-400 focus:outline-none"
           placeholder="Escriba su mensaje aquí"
+          required
         />
-        <button type="submit" className="p-2 bg-[#2C2C2C] hover:bg-gray-400 transition-all duration-700 drop-shadow opacity-80 text-xs w-full bg-opacity-10 border border-white border-opacity-20 rounded-lg">
-          Enviar
+        <button
+          type="submit"
+          className={`p-2 mt-2 ${sending ? 'bg-gray-600 cursor-not-allowed' : 'bg-[#2C2C2C] hover:bg-gray-400'} transition-all duration-700 text-xs w-full text-white border border-white border-opacity-20 rounded-lg`}
+          disabled={sending}
+        >
+          {sending ? 'Enviando...' : 'Enviar'}
         </button>
+        {success && <p className="text-green-500 mt-2">¡Mensaje enviado con éxito!</p>}
       </form>
     </div>
   );
