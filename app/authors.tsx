84c1068b6bedf6e8ddffe3758f648582b528c296
i9-9 <yvan.vrs@gmail.com>
2024-10-09 19:40:50 -0300

    text apt for jsx now

--- app/authors.tsx
+++ app/authors.tsx
@@ -4,15 +4,15 @@ const authors = [
         author: 'María Magdalena',
         description: (
             <>
-                <br></br>María Magdalena (1984, Buenos Aires). Poeta y escritora. Psicoanalista.
-                Editora en Las Furias.<br></br><br></br> Publicó los libros de poesía <em>Spleen</em> (2013, Letra
-                Viva), <em>Los nombres del padre</em> (2016, Buenos Aires Poetry), la plaquette
-                artesanal <em>La pequeña muerte</em> (2015), <em>Continente negro</em> (2018, Alción; 2021, el
-                diván negro –México–), <em>Un invierno sin Emma</em> (2022, Vagantes Fabulae), y los
-                ensayos <em>La perfecta desnudez. Conversaciones desde Alejandra
-                Pizarnik</em> (2018, Letra Viva) en co-autoría con Javier Galarza y Leonardo
-                Leibson, <em>Diario de la errancia. Elogio del viaje</em> (2020, La Docta Ignorancia) y <em>No
-                hay milagro más cruel que este. Sylvia Plath: amar, maternar, escribir</em> (2022,
+                <br />María Magdalena (1984, Buenos Aires). Poeta y escritora. Psicoanalista.
+                Editora en <i>Las Furias</i>.<br /><br />Publicó los libros de poesía <i>Spleen</i> (2013, Letra
+                Viva), <i>Los nombres del padre</i> (2016, Buenos Aires Poetry), la plaquette
+                artesanal <i>La pequeña muerte</i> (2015), <i>Continente negro</i> (2018, Alción; 2021, el
+                diván negro –México–), <i>Un invierno sin Emma</i> (2022, Vagantes Fabulae), y los
+                ensayos <i>La perfecta desnudez. Conversaciones desde Alejandra
+                Pizarnik</i> (2018, Letra Viva) en co-autoría con Javier Galarza y Leonardo
+                Leibson, <i>Diario de la errancia. Elogio del viaje</i> (2020, La Docta Ignorancia) y <i>No
+                hay milagro más cruel que este. Sylvia Plath: amar, maternar, escribir</i> (2022,
                 Las Furias).
             </>
         ),
@@ -23,30 +23,30 @@ const authors = [
         author: 'María Malusardi',
         description: (
             <>
-                <br></br>María Malusardi nació en Buenos Aires, Argentina, en 1966. Escritora, periodista y
-                docente. <br></br><br></br>Obra: <em>Una madre es un piano triste</em> (Las Furias Editora, 2021), <em>Artista del
-                hambre</em> (Ediciones en Danza, 2019), <em>El descenso de jacqueline du pré y otros
-                poemas</em> (Ediciones en Danza, 2018), <em>El desvío y el daño</em> (Cadáver exquisito, 2023
-                edición realizada en Ecuador y editorial Buenos Aires Poetry, 2017), <em>El sastre</em>
-                (Ediciones en Danza, 2015), <em>Artista del trapecio</em> (Alción, 2014), <em>La música</em> (El suri
-                porfiado, 2013), <em>El orfanato</em> (Alción, 2010), <em>Trilogía de la tristeza</em> (Alción, 2009),
-                <em> Museo de postales</em> (El suri porfiado, 2008), <em>Diálogo con pescadores</em> (Alción, 2007),
-                <em>Variaciones en la niebla</em> (Alción, 2005), <em>La carta de vermeer</em> (Alción, 2002) y <em>El
-                accidente</em> (Mascaró, 2001).
-                <br></br><br></br>Obtuvo por <em>El sastre</em>, la Mención especial del Premio de Literatura Casa de las
-                Américas 2015, de Cuba. Y por <em>Artista del hambre</em> el Segundo Premio Municipal de
-                Poesía 2018-2019. Y <em>Trilogía de la tristeza</em> resultó finalista del Concurso Olga
+                <br />María Malusardi nació en Buenos Aires, Argentina, en 1966. Escritora, periodista y
+                docente. <br /><br />Obra: <i>Una madre es un piano triste</i> (Las Furias Editora, 2021), <i>Artista del
+                hambre</i> (Ediciones en Danza, 2019), <i>El descenso de jacqueline du pré y otros
+                poemas</i> (Ediciones en Danza, 2018), <i>El desvío y el daño</i> (Cadáver exquisito, 2023
+                edición realizada en Ecuador y editorial Buenos Aires Poetry, 2017), <i>El sastre</i>
+                (Ediciones en Danza, 2015), <i>Artista del trapecio</i> (Alción, 2014), <i>La música</i> (El suri
+                porfiado, 2013), <i>El orfanato</i> (Alción, 2010), <i>Trilogía de la tristeza</i> (Alción, 2009),
+                <i>Museo de postales</i> (El suri porfiado, 2008), <i>Diálogo con pescadores</i> (Alción, 2007),
+                <i>Variaciones en la niebla</i> (Alción, 2005), <i>La carta de vermeer</i> (Alción, 2002) y <i>El
+                accidente</i> (Mascaró, 2001).
+                <br /><br />Obtuvo por <i>El sastre</i>, la Mención especial del Premio de Literatura Casa de las
+                Américas 2015, de Cuba. Y por <i>Artista del hambre</i> el Segundo Premio Municipal de
+                Poesía 2018-2019. Y <i>Trilogía de la tristeza</i> resultó finalista del Concurso Olga
                 Orozco 2009 que organizó la Universidad de San Martín, con un jurado integrado
                 por Antonio Gamoneda, Gonzalo Rojas, Juan Gelman y Jorge Boccanera; además
-                fue traducido al francés y editado en 2013 como <em>Trilogie de la tristesse</em> (Zinnia
+                fue traducido al francés y editado en 2013 como <i>Trilogie de la tristesse</i> (Zinnia
                 Editions), tanto en papel como en formato e-book.
-                <br></br><br></br> Recibió en 2018 la beca del Fondo Nacional de las Artes para escribir <em>Asamblea
-                permanente. Diálogos para una hermenéutica</em>, un ensayo sobre la obra y vida del
+                <br /><br /> Recibió en 2018 la beca del Fondo Nacional de las Artes para escribir <i>Asamblea
+                permanente. Diálogos para una hermenéutica</i>, un ensayo sobre la obra y vida del
                 poeta argentino Alberto Szpunberg.
-                <br></br><br></br>Estuvo a cargo de la edición y estudio preliminar de la poesía de Raúl Gustavo
-                Aguirre en el volumen <em>Obra poética</em> (Ediciones Del dock, 2015) y del ensayo <em>Las
-                poéticas del siglo XX</em> (Audisea, 2016). 
-                <br></br><br></br><em> Nadie sabe qué hacer con los poetas</em>, próximamente editado por Llantén, reúne sus
+                <br /><br />Estuvo a cargo de la edición y estudio preliminar de la poesía de Raúl Gustavo
+                Aguirre en el volumen <i>Obra poética</i> (Ediciones Del dock, 2015) y del ensayo <i>Las
+                poéticas del siglo XX</i> (Audisea, 2016). 
+                <br /><br /><i>Nadie sabe qué hacer con los poetas</i>, próximamente editado por Llantén, reúne sus
                 escritos sobre poesía, publicados e inéditos.
             </>
         ),
