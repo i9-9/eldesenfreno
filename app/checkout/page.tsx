f28c6f7a37e87fb2298a391895194acf9eede510
i9-9 <yvan.vrs@gmail.com>
2025-03-01 15:32:44 -0300

    config for mp not ready yet

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,41 +1,41 @@
-"use client"
+// "use client"
 
-import { useState } from 'react';
-import { useCarrito } from '../context/CarritoContext'
-export default function Checkout() {
-  const { productos } = useCarrito();
-  const [loading, setLoading] = useState(false);
+// import { useState } from 'react';
+// import { useCarrito } from '../context/CarritoContext'
+// export default function Checkout() {
+//   const { productos } = useCarrito();
+//   const [loading, setLoading] = useState(false);
 
-  const handleCheckout = async () => {
-    setLoading(true);
+//   const handleCheckout = async () => {
+//     setLoading(true);
 
-    try {
-      const response = await fetch('/api/mercadoPago', {
-        method: 'POST',
-        body: JSON.stringify({ productos }),
-      });
-      const data = await response.json();
+//     try {
+//       const response = await fetch('/api/mercadoPago', {
+//         method: 'POST',
+//         body: JSON.stringify({ productos }),
+//       });
+//       const data = await response.json();
 
-      // Redirigir a Mercado Pago
-      if (data.id) {
-        window.location.href = `https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=${data.id}`;
-      } else {
-        alert('Error al procesar el pago');
-      }
-    } catch (error) {
-      console.error(error);
-      alert('Hubo un error al iniciar el pago');
-    } finally {
-      setLoading(false);
-    }
-  };
+//       // Redirigir a Mercado Pago
+//       if (data.id) {
+//         window.location.href = `https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=${data.id}`;
+//       } else {
+//         alert('Error al procesar el pago');
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       alert('Hubo un error al iniciar el pago');
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
-  return (
-    <div>
-      <h1>Checkout</h1>
-      <button onClick={handleCheckout} disabled={loading}>
-        {loading ? 'Procesando...' : 'Pagar con Mercado Pago'}
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Checkout</h1>
+//       <button onClick={handleCheckout} disabled={loading}>
+//         {loading ? 'Procesando...' : 'Pagar con Mercado Pago'}
+//       </button>
+//     </div>
+//   );
+// }
